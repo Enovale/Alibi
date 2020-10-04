3f795f8a7ac1b0b7c0639bab4d8d01c4fffd60cf
Enova <17408285+Enovale@users.noreply.github.com>
2020-10-04 15:48:39 -0500

    message handlers now exist

---
+++ AO2Sharp/Protocol/MessageHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace AO2Sharp.Protocol
+{
+    internal static class MessageHandler
+    {
+        private static readonly Dictionary<string, Handler> _handlers = new Dictionary<string, Handler>();
+
+        internal delegate void Handler(Server server, string message);
+
+        static MessageHandler()
+        {
+            AddHandlers();
+        }
+
+        public static void HandleMessage(Server server, string message)
+        {
+            string messageType = message.Split("#").First();
+
+            if (_handlers.ContainsKey(messageType))
+            {
+                _handlers[messageType](server, message);
+            }
+            else
+            {
+                Console.WriteLine($"Dispatcher: Unknown client message '{messageType}'");
+            }
+        }
+
+        public static void RegisterMessageHandler(string messageName, Handler handler)
+        {
+            if (!_handlers.ContainsKey(messageName))
+                _handlers.Add(messageName, handler);
+
+            _handlers[messageName] = handler;;
+        }
+
+        public static void AddHandlers()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetRuntimeMethods();
+                foreach (var method in methods)
+                {
+                    if (method.IsStatic)
+                    {
+                        var attr = method.GetCustomAttribute<MessageHandlerAttribute>();
+
+                        if (attr != null)
+                            RegisterMessageHandler(attr.MessageName, (Handler)method.CreateDelegate(typeof(Handler)));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ AO2Sharp/Protocol/MessageHandlerAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AO2Sharp.Protocol
+{
+    [AttributeUsage(AttributeTargets.Method)]
+    public class MessageHandlerAttribute : Attribute
+    {
+        public string MessageName { get; }
+
+        public MessageHandlerAttribute(string messageName)
+            => MessageName = messageName;
+    }
+}
