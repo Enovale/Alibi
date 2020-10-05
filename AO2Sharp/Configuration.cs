1c1431ebbb7b5c7d5052b5cd9e38cd687991cfa1
Elijah Bansley <17408285+ElijahZAwesome@users.noreply.github.com>
2020-10-05 18:19:31 -0500

    Learn how the libraries you use work before you use them, elijah.

--- AO2Sharp/Configuration.cs
+++ AO2Sharp/Configuration.cs
@@ -12,21 +12,21 @@ namespace AO2Sharp
     [Serializable]
     internal class Configuration
     {
-        public string ServerName { get; private set; } = "Test Server";
-        public string ServerDescription { get; private set; } = "Example server description.";
-        public string ModPassword { get; private set; } = "ChangeThis";
+        public readonly string ServerName = "Test Server";
+        public readonly string ServerDescription = "Example server description.";
+        public readonly string ModPassword = "ChangeThis";
 
-        public IPAddress BoundIpAddress { get; private set; } = IPAddress.Parse("0.0.0.0");
-        public int Port { get; private set; } = 27016;
-        public int WebsocketPort { get; private set; } = 27017;
-        public string MasterServerAddress { get; private set; } = "master.aceattorneyonline.com";
-        public int MasterServerPort { get; private set; } = 27016;
+        public readonly IPAddress BoundIpAddress = IPAddress.Parse("0.0.0.0");
+        public readonly int Port = 27016;
+        public readonly int WebsocketPort = 27017;
+        public readonly string MasterServerAddress = "master.aceattorneyonline.com";
+        public readonly int MasterServerPort = 27016;
 
-        public bool Advertise { get; private set; } = true;
-        public int MaxPlayers { get; private set; } = 100;
-        public int TimeoutSeconds { get; private set; } = 60;
+        public readonly bool Advertise = true;
+        public readonly int MaxPlayers = 100;
+        public readonly int TimeoutSeconds = 60;
 
-        public string[] FeatureList { get; private set; } =
+        public readonly string[] FeatureList =
         {
             "yellowtext", "prezoom", "flipping", "customobjections",
             "deskmod", "evidence", "cccc_ic_support",
