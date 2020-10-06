4deb46d91edab220c977688d3c45d1dab04c0cbd
Elijah Bansley <17408285+ElijahZAwesome@users.noreply.github.com>
2020-10-06 10:51:41 -0500

    We got commands boys

---
+++ AO2Sharp/Commands/CommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using AO2Sharp.Helpers;
+using AO2Sharp.Protocol;
+
+namespace AO2Sharp.Commands
+{
+    internal static class CommandHandler
+    {
+        internal static readonly Dictionary<string, Handler> _handlers = new Dictionary<string, Handler>();
+
+        internal delegate void Handler(Client client, string[] args);
+
+        static CommandHandler()
+        {
+            AddHandlers();
+        }
+
+        public static void HandleMessage(Client client, string command, string[] args)
+        {
+            if (_handlers.ContainsKey(command))
+            {
+                _handlers[command](client, args);
+            }
+            else
+            {
+                client.SendOocMessage("Unknown command. Type /help for a list of commands.");
+            }
+        }
+
+        public static void RegisterMessageHandler(string messageName, Handler handler)
+        {
+            if (!_handlers.ContainsKey(messageName))
+                _handlers.Add(messageName, handler);
+
+            _handlers[messageName] = handler; ;
+        }
+
+        public static void AddHandlers()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes();
+            foreach (var type in types)
+            {
+                var methods = type.GetRuntimeMethods();
+                foreach (var method in methods)
+                {
+                    if (method.IsStatic)
+                    {
+                        var attr = method.GetCustomAttribute<CommandHandlerAttribute>();
+
+                        if (attr != null)
+                            RegisterMessageHandler(attr.Command, (Handler)method.CreateDelegate(typeof(Handler)));
+                    }
+                }
+            }
+        }
+    }
+}
--- AO2Sharp/Commands/Commands.cs
+++ AO2Sharp/Commands/Commands.cs
@@ -1,6 +1,19 @@
-﻿namespace AO2Sharp.Commands
+﻿using System.Linq;
+using System.Reflection;
+using AO2Sharp.Protocol;
+
+namespace AO2Sharp.Commands
 {
-    class Commands
+    internal static class Commands
     {
+        [CommandHandler("help", "Show's this text.")]
+        internal static void Help(Client client, string[] args)
+        {
+            string finalResponse = "Commands: \n";
+            foreach (var (command, description) in CommandHandler._handlers)
+                finalResponse += $"/{command}: {description.Method.GetCustomAttributes<CommandHandlerAttribute>().First().ShortDesc}";
+
+            client.SendOocMessage(finalResponse);
+        }
     }
 }
