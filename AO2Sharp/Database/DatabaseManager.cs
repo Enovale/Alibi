b91f428d61c79d0ca695a49015f2be693ae25da6
Elijah Bansley <17408285+ElijahZAwesome@users.noreply.github.com>
2020-10-09 13:11:44 -0500

    Small database boilerplate

---
+++ AO2Sharp/Database/DatabaseManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using SQLite;
+
+namespace AO2Sharp.Database
+{
+    public class DatabaseManager
+    {
+        public const string DatabaseFolder = "DatabaseManager";
+        public static readonly string DatabasePath = Path.Combine(DatabaseFolder, "database.db");
+
+        private SQLiteAsyncConnection _sql;
+
+        public DatabaseManager()
+        {
+            _sql = new SQLiteAsyncConnection(DatabasePath);
+        }
+    }
+}
